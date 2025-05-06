2a05339c2ef5876bd4b7c9aac0d7c355f0538436
Surya200901 <settipallisurya1111@gmail.com>
2025-05-06 11:20:34 +0530

    Add third Thread in the program

--- src/main/java/com/thread/prog/Demo.java
+++ src/main/java/com/thread/prog/Demo.java
@@ -27,12 +27,16 @@ public class Demo {
  public static void main(String[] args) {
      Runnable r1 = new MyRunnable("Thread-1");
      Runnable r2 = new MyRunnable("Thread-2");
+     Runnable r3 = new MyRunnable("Thread-3");
 
      Thread t1 = new Thread(r1);
      Thread t2 = new Thread(r2);
+     Thread t3 = new Thread(r3);
 
      t1.start(); // Start first thread
      t2.start(); // Start second thread
+     t3.start();
+     
  }
 }
 
