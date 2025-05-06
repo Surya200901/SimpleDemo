ff19abb8391d3aae5c0726bba94a205df1193f86
Surya200901 <settipallisurya1111@gmail.com>
2025-05-06 10:53:16 +0530

    Simple Thread Program

---
+++ src/main/java/com/thread/prog/Demo.java
@@ -0,0 +1,38 @@
+package com.thread.prog;
+
+//ThreadDemo.java
+
+class MyRunnable implements Runnable {
+ private String threadName;
+
+ public MyRunnable(String threadName) {
+     this.threadName = threadName;
+ }
+
+ @Override
+ public void run() {
+     for (int i = 1; i <= 5; i++) {
+         System.out.println(threadName + " - Count: " + i);
+         try {
+             Thread.sleep(500); // pause for 0.5 seconds
+         } catch (InterruptedException e) {
+             System.out.println(threadName + " interrupted.");
+         }
+     }
+     System.out.println(threadName + " finished execution.");
+ }
+}
+
+public class Demo {
+ public static void main(String[] args) {
+     Runnable r1 = new MyRunnable("Thread-1");
+     Runnable r2 = new MyRunnable("Thread-2");
+
+     Thread t1 = new Thread(r1);
+     Thread t2 = new Thread(r2);
+
+     t1.start(); // Start first thread
+     t2.start(); // Start second thread
+ }
+}
+
